dea892873ae5d13d37e9eb831cbdda8411bce8e5
Pri23p <br10204160324@gmail.com>
2026-01-26 03:00:38 +0530

    project

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+  REACT: "/vibe-starters/react-ts",
+  NEXTJS: "/vibe-starters/nextjs-new",
+  EXPRESS: "/vibe-starters/express-simple",
+  VUE: "/vibe-starters/vue",
+  HONO: "/vibe-starters/hono-nodejs-starter",
+  ANGULAR: "/vibe-starters/angular",
+};
